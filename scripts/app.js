b59d2034b761e862ca25b70063ab63c39be10199
Angela W <squittenplease@gmail.com>
2022-05-17 19:18:38 -0700

    fix scripts

--- scripts/app.js
+++ scripts/app.js
@@ -2,25 +2,28 @@ const timer = document.querySelector('#timer');
 const randomWordEle = document.getElementById("random-word");
 const userInputIEle = document.querySelector('#user-guess');
 const nextBtn = document.querySelector('#btn');
-
+const startBtn = document.querySelector('#startButton')
 let userScore = 0;
-//this is a place holder until i want to use the real api, essientally calls a fake word
-let randomindex = Math.floor(Math.random() * 100)
-//btn to start the game
+
+startBtn.addEventListener("click", () => {
+    getRandomWordCall();
+})
+
+
 nextBtn.addEventListener('click', e => {
     e.preventDefault()
-    userInputIEle.innerText = ""
+    userInputIEle.innerText = " "
     getRandomWordCall();
     getUserGuess();
 })
-//make api call and grab random word
+
 function getRandomWordCall() {
-    let randomURL = `https://jsonplaceholder.typicode.com/posts/${randomindex}`
+    let randomURL = "https://random-word-api.herokuapp.com/word?length=7"
     fetch(randomURL)
         .then(response => response.json())
         .then(data => {
             console.log(data);
-            const randomWord = data.title;
+            const randomWord = data;
             displayRandomWord(randomWord);
             // searchRandomPic(randomWord);
         })
@@ -28,8 +31,12 @@ function getRandomWordCall() {
 }
 // get random word and store in local storage
 function displayRandomWord(word) {
-    randomWordEle.innerText = word
     localStorage.setItem("randomWord", word)
+    randomWordEle.innerText = word
+    setTimeout(() => {
+        randomWordEle.innerText = ""
+    }, 1000)
+
 }
 // get the users guess - add event listener? 
 function getUserGuess() {
@@ -45,16 +52,29 @@ function compareWords(userWord, randomWord) {
     userWord = userWord.toUpperCase();
     if (randomWord === userWord) {
         console.log("YOU WIN!");
+        document.querySelector("#user-guess").classList.add("correct")
         userScore++;
         document.getElementById('final-score').innerText = userScore
+        correctGuess()
         // add score here
     } else {
         console.log("YOU LOSE!");
-
+        wrongGuess()
     }
-    randomindex = Math.floor(Math.random() * 100)
-
 }
 
 
 // test test test
+function correctGuess() {
+    setTimeout(() => {
+        document.querySelector("#user-guess").classList.add("correct");
+        document.querySelector("#input-label").innerText = "Correct ✅"
+    }, 2000)
+}
+
+function wrongGuess() {
+    setTimeout(() => {
+        document.querySelector("#user-guess").classList.add("wrong")
+        document.querySelector("#input-label").innerText = "Wrong ⛔"
+    }, 2000)
+}
