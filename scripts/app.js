576b95ad5a2770cc6e3336aa29f0fd4168a072ab
Angela W <squittenplease@gmail.com>
2022-05-19 20:49:50 -0700

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -61,6 +61,7 @@ function getRandomWordCall(length) {
             console.log(data);
             const randomWord = data;
             displayRandomWord(randomWord);
+            getAudio(randomWord)
             // getPic(randomWord[0]);
             // searchRandomPic(randomWord);
         })
@@ -70,29 +71,40 @@ function getRandomWordCall(length) {
 
 // api for images
 function imagesearch(randomWord) {
-    const url = "https://api.pexels.com/v1/search?query="+randomWord+"&per_page=1";
+    const url = "https://api.pexels.com/v1/search?query=" + randomWord + "&per_page=1";
     fetch(url, {
-      method: "GET",
-      withCredentials: true,
-      headers: {
-        "Authorization": "563492ad6f91700001000001524417ab089d4a73998c49062467e805",
-        "Content-Type": "application/json"
-      }
+        method: "GET",
+        withCredentials: true,
+        headers: {
+            "Authorization": "563492ad6f91700001000001524417ab089d4a73998c49062467e805",
+            "Content-Type": "application/json"
+        }
     })
-    .then(resp => resp.json())
-    .then(function(data) {
-        const photo = data.photos;
-        // displayimage(photo);
-        var randomImage = photo[0].src.original
-        console.log(randomImage)
-        document.querySelector('#image-container').innerHTML = `<img src=${randomImage} alt=${randomImage}>`
-    })
-    .catch(function(error) {
-        console.log(error);
-    });
+        .then(resp => resp.json())
+        .then(function (data) {
+            const photo = data.photos;
+            // displayimage(photo);
+            var randomImage = photo[0].src.original
+            console.log(randomImage)
+            document.querySelector('#image-container').innerHTML = `<img src=${randomImage} alt=${randomImage}>`
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 }
 // get random word and store in local storage
 
+
+function getAudio(searchTerm) {
+    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${searchTerm}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            const wordAudio = data[0].phonetics[0].audio;
+            console.log(wordAudio);
+
+        })
+}
 
 //Display the word for set time- I wanted emojis ⬅️🤷🏽‍♂️
 
